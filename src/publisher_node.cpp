e90eb57b865f531d42217511ee1ed25e5cf036a6
Sujith Malladi <sujithmalladi@gmail.com>
2026-01-31 12:01:40 +0530

    Completed ROS2 publisher Assignent

--- src/publisher_node.cpp
+++ src/publisher_node.cpp
@@ -8,58 +8,40 @@
 
 using namespace std::chrono_literals;
 
-/*
- * Create a Class named 'PublisherNode' that inherits from rclcpp::Node.
- * Requirements:
- * 1. The constructor should name the node "publisher_node".
- * 2. Create a publisher to topic "/counter" with message type std_msgs::msg::String
- * 3. Create a timer that triggers every 500ms.
- * 4. The timer callback should:
- *    - Increment a counter (starting from 0)
- *    - Create a message with format "Count: X" where X is the counter
- *    - Publish the message
- */
-
 class PublisherNode : public rclcpp::Node
 {
 public:
-    PublisherNode()
-        : Node("publisher_node"), count_(0)
-    {
-        // Create the publisher
-        publisher_ = this->create_publisher<std_msgs::msg::String>(
-            "/counter", 10);
+  PublisherNode()
+  : Node("publisher_node"), count_(0)
+  {
+    publisher_ = this->create_publisher<std_msgs::msg::String>(
+      "/counter", 10);
 
-        // Create the timer (500 ms)
-        timer_ = this->create_wall_timer(
-            500ms,
-            std::bind(&PublisherNode::timer_callback, this));
-    }
+    timer_ = this->create_wall_timer(
+      500ms,
+      std::bind(&PublisherNode::timer_callback, this));
+  }
 
 private:
-    // Timer callback function
-    void timer_callback()
-    {
-        std_msgs::msg::String message;
-        message.data = "Count: " + std::to_string(count_++);
+  void timer_callback()
+  {
+    std_msgs::msg::String msg;
+    msg.data = "Count: " + std::to_string(count_++);
+    publisher_->publish(msg);
 
-        publisher_->publish(message);
+    RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", msg.data.c_str());
+  }
 
-        RCLCPP_INFO(this->get_logger(),
-                    "Publishing: '%s'",
-                    message.data.c_str());
-    }
-
-    rclcpp::TimerBase::SharedPtr timer_;
-    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
-    size_t count_;
+  rclcpp::TimerBase::SharedPtr timer_;
+  rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
+  size_t count_;
 };
 
-int main(int argc, char *argv[])
+int main(int argc, char * argv[])
 {
-    rclcpp::init(argc, argv);
-    rclcpp::spin(std::make_shared<PublisherNode>());
-    rclcpp::shutdown();
-    return 0;
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<PublisherNode>());
+  rclcpp::shutdown();
+  return 0;
 }
 
